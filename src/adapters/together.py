a3c2c97ae4e87470cd8012bb568bd44104754e01
Kutsev, Dmitry <dmitry.kutsev@kpn.com>
2025-02-22 13:27:33 +0100

    FIX: added time measurement, restarted interface

--- src/adapters/together.py
+++ src/adapters/together.py
@@ -8,7 +8,7 @@ class TogetherAdapter(LLMAdapter):
     def __init__(self):
         super().__init__()
         model_name = self.settings.DEFAULT_TOGETHER_LLM
-        self.client = Together(model=model_name, api_key=self.settings.TOGETHER_API_KEY)  # type: ignore https://github.com/pydantic/pydantic/issues/9557
+        self.client = Together(model=model_name, api_key=self.settings.TOGETHER_API_KEY, max_tokens=200)  # type: ignore https://github.com/pydantic/pydantic/issues/9557
 
     @traceable
     def generate(self, prompt: str) -> str:
